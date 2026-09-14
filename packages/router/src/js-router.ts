4fe85f5e0f4fa8260c348420072e3076a6e38c01
ShaneKoNaung <shanekonaung@gmail.com>
2026-09-14 11:35:08 +0700

    style: format router compatibility facade

--- packages/router/src/js-router.ts
+++ packages/router/src/js-router.ts
@@ -24,13 +24,7 @@ export class JsRouter implements Autorouter {
     };
   }
 
-  async route(
-    _input: RouteInput,
-    _opts: RouteOptions = {},
-    _progress?: (progress: RouteProgress) => void,
-  ): Promise<RouteResult> {
-    throw new Error(
-      "the in-tab capacity router was removed; choose the server-side JavaScript router",
-    );
+  async route(_input: RouteInput, _opts: RouteOptions = {}, _progress?: (progress: RouteProgress) => void): Promise<RouteResult> {
+    throw new Error("the in-tab capacity router was removed; choose the server-side JavaScript router");
   }
 }
